5253dbd834e730b9f7f884565435337db370a239
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2020-03-03 19:38:27 +0100

    Add versions and skip Initialize for MSSQL

--- src/DatabaseProvider/Repositories/Sql/SqlUnturnedItemAssetsRepository.cs
+++ src/DatabaseProvider/Repositories/Sql/SqlUnturnedItemAssetsRepository.cs
@@ -89,10 +89,9 @@ namespace Marketplace.DatabaseProvider.Repositories.Sql
 
         }
 
-        public async Task Initialize()
+        public Task Initialize()
         {
-            const string sql = "CREATE TABLE [dbo].[UnturnedItems](ItemId INT NOT NULL CONSTRAINT PK_UnturnedItems PRIMARY KEY, ItemName NVARCHAR(255) NOT NULL, ItemType VARCHAR(255) NOT NULL, ItemDescription NVARCHAR(800) NOT NULL, Amount TINYINT NOT NULL CONSTRAINT DF_UnturnedItems_Amount DEFAULT 1, Icon VARBINARY(MAX) NULL)";
-            await connection.ExecuteAsync(sql).ConfigureAwait(false);
+            return Task.CompletedTask;
         }
     }
 }
