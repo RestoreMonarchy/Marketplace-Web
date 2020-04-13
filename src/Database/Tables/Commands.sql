e69a3c66985bc89bdb92dd688bd03c094f4883ba
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2020-04-13 23:43:01 +0200

    Fix NavBar show dashboard and Expires for commands

--- src/Database/Tables/Commands.sql
+++ src/Database/Tables/Commands.sql
@@ -4,7 +4,7 @@
 	CommandName NVARCHAR(50) NOT NULL CONSTRAINT UK_Commands UNIQUE,
 	CommandHelp NVARCHAR(255) NOT NULL,
 	CommandText NVARCHAR(255) NOT NULL,
-	Expires AS CAST(ExpireTime AS BIT),
+	Expires BIT NOT NULL,
 	ExpireTime INT NOT NULL,
 	ExpireCommand NVARCHAR(255) NULL,
 	ExecuteOnBuyerJoinServer BIT NOT NULL
--- src/DatabaseProvider/Repositories/Sql/SqlCommandsRepository.cs
+++ src/DatabaseProvider/Repositories/Sql/SqlCommandsRepository.cs
@@ -22,15 +22,15 @@ namespace Marketplace.DatabaseProvider.Repositories.Sql
 
         public async Task<int> AddCommandAsync(Command command)
         {
-            const string sql = "INSERT INTO dbo.Commands (CommandName, CommandHelp, CommandText, ExpireTime, ExpireCommand, ExecuteOnBuyerJoinServer) " +
-                "VALUES (@CommandName, @CommandHelp, @CommandText, @ExpireTime, @ExpireCommand, @ExecuteOnBuyerJoinServer); SELECT SCOPE_IDENTITY();";
+            const string sql = "INSERT INTO dbo.Commands (CommandName, CommandHelp, CommandText, Expires, ExpireTime, ExpireCommand, ExecuteOnBuyerJoinServer) " +
+                "VALUES (@CommandName, @CommandHelp, @CommandText, @Expires, @ExpireTime, @ExpireCommand, @ExecuteOnBuyerJoinServer); SELECT SCOPE_IDENTITY();";
             return await connection.ExecuteScalarAsync<int>(sql, command);
         }
 
         public async Task UpdateCommandAsync(Command command)
         {
-            const string sql = "UPDATE dbo.Commands SET CommandName = @CommandName, CommandHelp = @CommandHelp, CommandText = @CommandText, ExpireTime = @ExpireTime, " +
-                "ExpireCommand = @ExpireCommand, ExecuteOnBuyerJoinServer = @ExecuteOnBuyerJoinServer WHERE Id = @Id;";
+            const string sql = "UPDATE dbo.Commands SET CommandName = @CommandName, CommandHelp = @CommandHelp, CommandText = @CommandText, Expires = @Expires, " +
+                "ExpireTime = @ExpireTime, ExpireCommand = @ExpireCommand, ExecuteOnBuyerJoinServer = @ExecuteOnBuyerJoinServer WHERE Id = @Id;";
             await connection.ExecuteAsync(sql, command);
         }
     }
